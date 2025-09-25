a0653d27979f1fad9ab05b06967e90c9935f1a66
alderfurtado <aldermarcel12@gmail.com>
2025-09-25 16:38:43 -0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	apiName := os.Getenv("API_NAME")
+	if apiName == "" {
+		apiName = "API_UNKNOW"
+	}
+
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.IndentedJSON(http.StatusOK, gin.H{
+			"result": apiName,
+		})
+	})
+
+	router.GET("/healtcheck", func(ctx *gin.Context) {
+		ctx.IndentedJSON(http.StatusOK, gin.H{
+			"result": "ok",
+		})
+	})
+
+	router.Run(":8080")
+}
